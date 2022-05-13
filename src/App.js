3f4ee7c3ce306a35089cdf4b08aabb8d5fb0cd99
Uday <shankhar87@gmail.com>
2022-05-13 12:48:33 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import data from "./data.json"
+
+function Node({ data, path }) {
+  if (Array.isArray(data)) {
+    return (
+      <ul>
+        {data.map((item, i) => (
+          <li key={i}>
+            <Node data={item} path={path + `[${i}]`} />
+          </li>
+        ))}
+      </ul>
+    );
+  } else if (typeof data === "object") {
+    return (
+      <div>
+        <ul>
+          {Object.entries(data).map(([key, item]) => (
+            <li key={key}>
+              {typeof item === "object" && "⯈"}
+              {key}: <Node data={item} path={path + `.${key}`} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else return <span>{data}</span>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 style={{ marginLeft: '80px' }}>React Dynamic Tree View</h3>
+      <Node data={data} path="data" />
     </div>
   );
 }
-
 export default App;
